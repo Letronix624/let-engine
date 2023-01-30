68e6539e9d3c820a97eee91633bb99f8169760fb
Let <94926994+Letronix624@users.noreply.github.com>
2023-01-30 14:47:47 +0100

    Started the library.

--- src/old/consts.rs
+++
@@ -1,5 +0,0 @@
-pub const WIDTH: u16 = 1000;
-pub const HEIGHT: u16 = 1000;
-pub const TITLE: &str = "Let - Vulkan";
-pub const APPNAME: &str = "Let Engine Test";
-pub const D_PRESENCE: u64 = 1066394163240767520;
